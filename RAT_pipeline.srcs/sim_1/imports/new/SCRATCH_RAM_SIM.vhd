8d8c58a406105d976f7ede1af490c3d0ec9638e6
Matthew Gerken <gerkenmatt@gmail.com>
2018-04-25 22:38:48 -0700

    Revert "asdfklsdfkjsdf"
    This reverts commit c714d2a4ff430e1dbe26622a7d6220f2b64f235a.

--- RAT_pipeline.srcs/sim_1/imports/new/SCRATCH_RAM_SIM.vhd
+++ RAT_pipeline.srcs/sim_1/imports/new/SCRATCH_RAM_SIM.vhd
@@ -69,8 +69,7 @@ uut: RAT_CPU
       RST_tb <= '1';
       wait for 10 ns;
       RST_tb <= '0';
---      wait for 3600 ns;    
-      wait for 1 ms;
+      wait for 3600 ns;    
 --      INT_IN_tb <= '1';
 --      wait for 30 ns;
 --      INT_IN_tb <= '0';
