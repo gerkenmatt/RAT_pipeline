5340d6ac08a2d01097be20796bb5a746f2495876
stuff4school <38698745+stuff4school@users.noreply.github.com>
2018-05-10 12:59:35 -0700

    hello motto
    Can you hear me now?

--- RAT_pipeline.sim/sim_1/behav/xsim/xsim.dir/CPU_SIM_behav/webtalk/xsim_webtalk.tcl
+++ RAT_pipeline.sim/sim_1/behav/xsim/xsim.dir/CPU_SIM_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/Carmelo/Documents/GitHub/RAT_pipeline/RAT_pipeline.sim/sim_1/behav/xsim/xsim.dir/CPU_SIM_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Thu May 10 12:43:36 2018" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Thu May 10 12:58:51 2018" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2017.4 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2086221" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "05a4975d-1982-4b64-9bfe-6c07a5dd4bd0" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "63dcda8d7ad74bb68223be1d407c8494" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "337" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "338" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i5-4210U CPU @ 1.70GHz" -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "11 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "4" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.09_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "7996_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 298764498 -regid "" -xml C:/Users/Carmelo/Documents/GitHub/RAT_pipeline/RAT_pipeline.sim/sim_1/behav/xsim/xsim.dir/CPU_SIM_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/Carmelo/Documents/GitHub/RAT_pipeline/RAT_pipeline.sim/sim_1/behav/xsim/xsim.dir/CPU_SIM_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/Carmelo/Documents/GitHub/RAT_pipeline/RAT_pipeline.sim/sim_1/behav/xsim/xsim.dir/CPU_SIM_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.03_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "8068_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 361752829 -regid "" -xml C:/Users/Carmelo/Documents/GitHub/RAT_pipeline/RAT_pipeline.sim/sim_1/behav/xsim/xsim.dir/CPU_SIM_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/Carmelo/Documents/GitHub/RAT_pipeline/RAT_pipeline.sim/sim_1/behav/xsim/xsim.dir/CPU_SIM_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/Carmelo/Documents/GitHub/RAT_pipeline/RAT_pipeline.sim/sim_1/behav/xsim/xsim.dir/CPU_SIM_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
