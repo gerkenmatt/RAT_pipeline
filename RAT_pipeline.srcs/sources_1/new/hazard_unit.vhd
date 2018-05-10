91b64ac31182359d095d62a4b77d83e6375207fa
stuff4school <38698745+stuff4school@users.noreply.github.com>
2018-05-10 09:15:24 -0700

    more stuff done
    ohhhhhhhhhhhhhhhhhhhh

--- RAT_pipeline.srcs/sources_1/new/hazard_unit.vhd
+++ RAT_pipeline.srcs/sources_1/new/hazard_unit.vhd
@@ -59,7 +59,7 @@ signal s_temp_instr           : STD_LOGIC_VECTOR(17 downto 0) := (others => '0')
 signal s_b_p2_clk               : STD_LOGIC := '1';
 signal s_d_p2_clk               : STD_LOGIC := '1';
 signal s_p2_clk               : STD_LOGIC := '1';
-signal branch_done            : STD_LOGIC := '0';
+--signal branch_done            : STD_LOGIC := '0';
 --signal data_run            : STD_LOGIC := '0';
 signal s_pc_ld              : STD_LOGIC_VECTOR(1 downto 0) := (others => '0');
 signal ophi : STD_LOGIC_VECTOR(4 downto 0):= (others => '0');
@@ -106,31 +106,24 @@ begin
                 s_temp_instr(1 downto 0) <= "11";
                 s_prev_instr <= PREV_INSTR;
                 data_flag <= "10";
+            elsif (branch_flag = "10") then  
+                    PC_CLK <= '1'; 
+--                  branch_done <= '1';
+                    branch_flag <= "00";
+                    s_prev_instr <= PREV_INSTR;
+            elsif (branch_flag = "01") then
+                if (PC_LD = '1') then
+                    s_pc_ld <= "01";
+                    s_temp_instr(17 downto 13) <= "11111";--no op 
+                    s_temp_instr(1 downto 0) <= "11";
+                    s_prev_instr <= PREV_INSTR;
+                end if;
+                PC_CLK <= '0';
+                branch_flag <= "10";
             else
                 PC_CLK <= '1';
                 B1_CLK <= '1';
             end if;
-            if (branch_ind = '1') then 
-                            if (branch_flag = "10") then  
-                                PC_CLK <= '1'; 
-                                branch_done <= '1';
-                                branch_flag <= "00";
-                                s_prev_instr <= PREV_INSTR;
-                            elsif (branch_flag = "01") then
-                                if (PC_LD = '1') then
-                                    s_pc_ld <= "01";
-                                    s_temp_instr(17 downto 13) <= "11111";--no op 
-                                    s_temp_instr(1 downto 0) <= "11";
-                                    s_prev_instr <= PREV_INSTR;
-                                end if;
-                                PC_CLK <= '0';
-                                branch_flag <= "10";
-                            else
-                                PC_CLK <= '0';
-                                branch_done <= '0';
-                                branch_flag <= "01";
-                            end if;
-                      end if;
             if (data_ind = '1') then
                     PC_CLK <= '0';
                     B1_CLK <= '0';
@@ -138,19 +131,23 @@ begin
                     s_temp_instr(1 downto 0) <= "11";
                     data_flag <= "01";
             end if;
+            if (branch_ind = '1') then
+                    PC_CLK <= '0';
+--                    branch_done <= '0';
+                    branch_flag <= "01";
+            end if;
         end if;
     end process stall;
     
     comb: process(CLK)
             begin
             if(FALLING_EDGE(CLK)) then
-                if (((OP_HI = "00100") or (OP_HI = "00101") or (OP_HI = "01100")) and (data_flag = "00")) then
+                if (((OP_HI = "00100") or (OP_HI = "00101") or (OP_HI = "01100")) and (data_flag = "00") and 
+                (branch_flag = "00")) then
                     s_b_p2_clk <= '0';
                     branch_ind <= '1';
                 end if;
-            end if;
-            if(RISING_EDGE(CLK)) then
-                if (branch_done = '1' and branch_flag="00") then
+                if (branch_flag="01") then
                    s_b_p2_clk <= '1';
                    branch_ind <='0';
                end if;
@@ -162,7 +159,9 @@ begin
             begin
             if(FALLING_EDGE(CLK)) then
                 if (((src_reg = dst_reg) or (cur_dst_reg = dst_reg)) and PREV_INSTR /= "000000000000000000" and
-                data_flag = "00") then
+                (data_flag = "00") and (branch_flag = "00") and (OP_HI /="11011") and (PREV_INSTR(17 downto 13) /= "00100")
+                and (PREV_INSTR(17 downto 13) /= "00101") and (OP_HI /="00100") and (OP_HI /="00101") and
+                (OP_HI /="01100")) then
                     s_d_p2_clk <= '0';
                     data_ind <= '1';
                 end if;
